fe9e3cd621696b84b5f765f0d15e885d4d52f003
madebyfabian <beer.fabian@mail.de>
2021-10-08 20:42:01 +0200

    remove tests

--- src/views/App/App-Test.vue
+++ src/views/App/App-Test.vue
@@ -1,27 +1,9 @@
 <template>
-	<TextInput 
-		v-model="searchNotesString" 
-		:inputProps="{ placeholder: 'Search for something', required: true, spellcheck: false }" 
-		class="mb-4">
-
-		<template #icon>
-			<IconSearch />
-		</template>
-	</TextInput>
+	Test
 </template>
 
 <script setup>
 	import { ref, computed } from 'vue'
-	import { useRoute, useRouter } from 'vue-router'
-
 	import { TextInput } from '@/components/ui'
 	import { IconSearch } from '@/assets/icons'
-
-	const route = useRoute(),
-				router = useRouter()	
-
-	const searchNotesString = computed({
-		get() 			{ return route.query?.q || '' },
-		set(newVal)	{ router.push({ query: { q: newVal } }) }
-	})
 </script>
