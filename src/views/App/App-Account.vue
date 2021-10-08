c612d0e419df0385378223ccbab1725fcdfd067a
madebyfabian <beer.fabian@mail.de>
2021-10-08 14:22:57 +0200

    styled account page

--- src/views/App/App-Account.vue
+++ src/views/App/App-Account.vue
@@ -1,9 +1,14 @@
 <template>
-	<h1 class="text-gray-300">Account</h1>
-	<button @click="router.push({ name: 'Auth-SignOut' })">Sign out</button>
+	<h1 class="text-gray-300 mb-4">Hej, {{ userName }}!</h1>
+	<Button @click="router.push({ name: 'Auth-SignOut' })">Sign out</Button>
 </template>
 
 <script setup>
+	import { computed } from 'vue'
 	import { useRouter } from 'vue-router'
+	import { Button } from '@/components/ui'
+	import { generalStore } from '@/store'
+
+	const userName = computed(() => generalStore.getUserName())
 	const router = useRouter()
 </script>
