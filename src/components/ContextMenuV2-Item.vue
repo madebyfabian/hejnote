e7212940d5ada77718e49fe54b4ff2ebcd0d521b
madebyfabian <beer.fabian@mail.de>
2021-10-09 13:16:28 +0200

    cleanup

--- src/components/ContextMenuV2-Item.vue
+++ src/components/ContextMenuV2-Item.vue
@@ -7,16 +7,11 @@
 </template>
 
 <script setup>
-	import { computed, useSlots } from 'vue'
 	import { MenuItem } from '@headlessui/vue'
 	import { Cell } from '@/components/ui'
-	import { IconCheck } from '@/assets/icons'
 
 	defineProps({
 		...MenuItem.props,
 		cellProps: { type: Object, default: () => ({}) },
 	})
-
-	const slots = useSlots()
-	const iconSlotVisible = computed(() => slots.icon?.())
 </script>
