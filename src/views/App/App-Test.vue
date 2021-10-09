1962ef11b27fbe6fd111ac2376ffd4e89826ffea
madebyfabian <beer.fabian@mail.de>
2021-10-09 15:24:30 +0200

    add store as test page default

--- src/views/App/App-Test.vue
+++ src/views/App/App-Test.vue
@@ -1,9 +1,10 @@
 <template>
-	Test
+	Hello World
 </template>
 
 <script setup>
 	import { ref, computed } from 'vue'
 	import { TextInput } from '@/components/ui'
 	import { IconSearch } from '@/assets/icons'
+	import { generalStore } from '@/store'
 </script>
