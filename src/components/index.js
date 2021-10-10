299938a00bd112ea1a08b242b5baba251948e2cc
madebyfabian <beer.fabian@mail.de>
2021-10-10 13:50:30 +0200

    add components index file

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+export { default as NoteActionBarCollection } from './Note-ActionBar-Collection.vue';
+export { default as NoteActionBar } from './Note-ActionBar.vue';
+export { default as NoteLinkList } from './Note-LinkList.vue';
+export { default as Note } from './Note.vue';
+export { default as NoteEditor } from './NoteEditor.vue';
+export { default as NoteListGrid } from './NoteList-Grid.vue';
+export { default as NoteList } from './NoteList.vue';
+export { default as Snackbar } from './Snackbar.vue';
+export { default as SnackbarContainer } from './SnackbarContainer.vue';
+export { default as SwitchHiddenMode } from './SwitchHiddenMode.vue';
