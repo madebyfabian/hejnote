21d141fa104adda93aa7114d1a65a1b68bba0fd8
madebyfabian <beer.fabian@mail.de>
2021-10-18 22:51:01 +0200

    added test page backin

---
+++ src/views/App/App-Test.vue
@@ -0,0 +1,18 @@
+<template>
+	<button @click="isLoading = !isLoading">Change loading</button>
+
+	<br><br>
+
+	<Button :isLoading="isLoading">
+		Test
+	</Button>
+</template>
+
+<script setup>
+	import { ref, computed } from 'vue'
+	import { generalStore, notesStore } from '@/store'
+	import { Button, ButtonIconOnly, LoadingSpinner } from '@/components/ui'
+	import { IconSearch } from '@/assets/icons'
+
+	const isLoading = ref(false)
+</script>
