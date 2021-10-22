d97c305a3d5515bbd19ecb6bb2fa8e9979079d38
madebyfabian <beer.fabian@mail.de>
2021-10-22 14:57:56 +0200

    fix file path for netlify

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import '@/assets/styles/tailwind.css'
 const app = createApp(App)
 
 // Import router and routes
-import { routes } from './routes.js'
+import { routes } from '@/routes'
 import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({ history: createWebHistory(), routes })
 
@@ -25,7 +25,7 @@ app.use(masonry)
 import vClickOutside from 'click-outside-vue3'
 app.use(vClickOutside)
 
-import BodyScrollLock from '@/plugins/bodyScrollLock.js'
+import BodyScrollLock from '@/plugins/BodyScrollLock'
 app.use(BodyScrollLock)
 
 app.mount('#app')
