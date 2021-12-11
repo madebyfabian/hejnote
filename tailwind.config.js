2fc17fc8cb8d9971584634114d7903f6269ce0c9
madebyfabian <beer.fabian@mail.de>
2021-12-11 09:56:55 +0100

    cleanup tailwind config before upgrade to v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 const plugin = require('tailwindcss/plugin')
-const lineClamp = require('@tailwindcss/line-clamp')
+const lineClampPlugin = require('@tailwindcss/line-clamp')
 
+const defaultBorderWidth = '1px'
 
-// Rotate X utilities
-const rotateX = plugin(function ({ addUtilities }) {
+const customUtilitiesPlugin = plugin(function ({ addUtilities, theme }) {
   addUtilities({
     '.rotate-x-180': {
       transform: 'rotateX(180deg)',
@@ -14,11 +14,60 @@ const rotateX = plugin(function ({ addUtilities }) {
       overflow: 'hidden',
       textOverflow: 'ellipsis',
       whiteSpace: 'nowrap',
-    }
+    },
+
+    '.content': {
+      content: 'attr(data-content)',
+    },
+
+    /**
+     * Add utilties to style checkboxes. This is needed, since our Richtext editor tiptap will not add <input type="checkbox">
+     * to the readonly mode HTML. so we have to use ::before and ::after to style the checkbox.
+     * These styles will then also be used for a component like Checkbox.vue 
+     */
+    '.checkbox': {
+      border: `${ defaultBorderWidth } solid ${ defaultTheme.colors.gray[600] }`,
+      borderRadius: defaultTheme.borderRadius.DEFAULT,
+      height: '1rem',
+      width: '1rem',
+      transitionDuration: '100ms',
+      transitionProperty: 'border-color, background-color',
+    },
+    '.checkbox-hoverable-el': {
+      borderRadius: defaultTheme.borderRadius.DEFAULT,
+      transitionDuration: '100ms',
+      transitionProperty: 'background-color',
+
+      '&:hover': {
+        background: defaultTheme.colors.gray[700]
+      }
+    },
+    '.checkbox-checked': {
+      background: defaultTheme.colors.gray[600],
+      borderColor: 'transparent'
+    },
+    '.checkbox-checked-icon': {
+      background: defaultTheme.colors.gray[400], // Icon color
+      mask: `url('@/assets/icons/special/check.svg') no-repeat center center`,
+      '-webkit-mask': `url('@/assets/icons/special/check.svg') no-repeat center center`,
+    },
+
+    /**
+     * Add utilities to style Marker element. This is needed (see above).
+     * This style will then also be used for a component like Marker.vue
+     */
+    '.marker': {
+      height: '0.5rem',
+      width: '0.5rem',
+      borderRadius: '100%',
+      border: `${ defaultBorderWidth } solid ${ defaultTheme.colors.gray[600] }`,
+    },
+
+    '.focus-visible': {
+      outline: 'none',
+    },
   })
 })
-
-const defaultBorderWidth = '1px'
 
 
 
@@ -130,70 +179,7 @@ module.exports = {
   },
 
   plugins: [
-    rotateX,
-    lineClamp,
-    plugin(({ addUtilities, theme }) => {
-      addUtilities({
-        '.content': {
-          content: 'attr(data-content)',
-        },
-
-        /**
-         * Add utilties to style checkboxes. This is needed, since our Richtext editor tiptap will not add <input type="checkbox">
-         * to the readonly mode HTML. so we have to use ::before and ::after to style the checkbox.
-         * These styles will then also be used for a component like Checkbox.vue 
-         */
-        '.checkbox': {
-          border: `${ defaultBorderWidth } solid ${ defaultTheme.colors.gray[600] }`,
-          borderRadius: defaultTheme.borderRadius.DEFAULT,
-          height: '1rem',
-          width: '1rem',
-          transitionDuration: '100ms',
-          transitionProperty: 'border-color, background-color',
-        },
-        '.checkbox-hoverable-el': {
-          borderRadius: defaultTheme.borderRadius.DEFAULT,
-          transitionDuration: '100ms',
-          transitionProperty: 'background-color',
-
-          '&:hover': {
-            background: defaultTheme.colors.gray[700]
-          }
-        },
-        '.checkbox-checked': {
-          background: defaultTheme.colors.gray[600],
-          borderColor: 'transparent'
-        },
-        '.checkbox-checked-icon': {
-          background: defaultTheme.colors.gray[400], // Icon color
-          mask: `url('@/assets/icons/special/check.svg') no-repeat center center`,
-          '-webkit-mask': `url('@/assets/icons/special/check.svg') no-repeat center center`,
-        },
-
-        /**
-         * Add utilities to style Marker element. This is needed (see above).
-         * This style will then also be used for a component like Marker.vue
-         */
-        '.marker': {
-          height: '0.5rem',
-          width: '0.5rem',
-          borderRadius: '100%',
-          border: `${ defaultBorderWidth } solid ${ defaultTheme.colors.gray[600] }`,
-        },
-
-        '.focus-visible': {
-          outline: 'none',
-        },
-
-        /**
-         * Icons
-         */
-        /*'.icon-link': {
-          background: defaultTheme.colors.gray[400], // Icon color
-          mask: `url('@/assets/icons/special/link.svg') no-repeat center center / 0.75rem`,
-          '-webkit-mask': `url('@/assets/icons/special/link.svg') no-repeat center center / 0.75rem`,
-        },*/
-      })
-    })
+    customUtilitiesPlugin,
+    lineClampPlugin
   ],
 }
