e2b44715b6d2b92583adfa72d6236adf567f1a88
madebyfabian <beer.fabian@mail.de>
2022-01-02 21:38:32 +0100

    new util type

--- src/env.d.ts
+++ src/env.d.ts
@@ -18,3 +18,8 @@ interface ImportMeta {
 }
 
 declare module 'click-outside-vue3';
+
+/**
+ * Makes all properties in T optional, excluding the ones you define in the type K.
+ */
+type PartialBy<T, K extends keyof T> = Partial<T> & Required<Pick<T, K>>
