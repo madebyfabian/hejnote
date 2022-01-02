0ad0f6d521492890d9f6aeb93cb9ce0b8a78059e
madebyfabian <beer.fabian@mail.de>
2022-01-02 11:38:38 +0100

    implemented more typescript with 0 code changes

--- src/hooks/useGetUrlHost.ts
+++ src/hooks/useGetUrlHost.ts
@@ -1,9 +1,9 @@
 /**
  * Takes in a full url and then returns...
- * @param {string} fullUrl Full url
- * @returns {string} The hostname of the current URL, e.g. "foo.example.com"
+ * @param fullUrl Full url (e.g. https://foo.example.com/my/path?cool=wow#is-this-a-hash)
+ * @returns The hostname of the current URL, e.g. "foo.example.com"
  */
-export default function useGetUrlHost( fullUrl ) {
+export default function useGetUrlHost( fullUrl: string ) {
 	let title
 	if (!fullUrl || fullUrl.length === 0 || fullUrl === '#' || fullUrl?.trim()?.length === 0)
 		return undefined
