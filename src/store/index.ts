52144003fe1d8eb9a6d7ef6bfcb99a1b4dabd7a5
madebyfabian <beer.fabian@mail.de>
2022-01-02 17:42:57 +0100

    cleanup

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,6 @@
 import collectionsStore from '@/store/collectionsStore'
 import generalStore from '@/store/generalStore'
 import joinNotesLinksStore from '@/store/joinNotesLinksStore'
-// @ts-ignore
 import linksStore from '@/store/linksStore'
 // @ts-ignore
 import notesStore from '@/store/notesStore'
